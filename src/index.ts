7e4f6376eebb00ce42f1deb9d3d9713a86f5e435
Syed Devop <syeds.devops007@gmail.com>
2023-07-09 21:51:51 +0530

    basi setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log("Hello Syed uzar");
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+import { NodeAudioVolumeMixer as volume } from "node-audio-volume-mixer";
+const PORT = 3000;
+
+interface ServerToClientEvents {}
+
+interface ClientToServerEvents {
+  isMute: () => boolean;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  user_id: string;
+}
+
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(PORT, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+console.log("Socket Server started @ http://localhost:" + PORT);
+
+instrument(io, {
+  auth: false,
+  mode: "development",
+});
+
+io.on("connection", (socket) => {
+  console.log(`socket ${socket.id} connected`);
+
+  socket.on("isMute", () => volume.isMasterMuted());
+
+  socket.on("disconnect", (reason) => {
+    console.log(`socket ${socket.id} disconnected due to ${reason}`);
+  });
+});
