318a193e377bce8efad021564e983a6c460944be
Syed Devop <syeds.devops007@gmail.com>
2023-07-13 14:29:55 +0530

    feat(app): add list vew for all the server
     - add showServerBottomSheet (Refs: #78e9546d).

--- pu_info_app/lib/screen/home_screen.dart
+++ pu_info_app/lib/screen/home_screen.dart
@@ -1,102 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-import '../models/server_model.dart';
+import '/widget/server/server_bottom_sheet.dart';
+import '/models/server_model.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Box<Server> serverBox;
+  @override
+  void initState() {
+    super.initState();
+    serverBox = Hive.box("server");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Pc Info")),
-      body: const Center(
-        child: Text("hello"),
+      body: Center(
+        child: ValueListenableBuilder(
+            valueListenable: serverBox.listenable(),
+            builder: (context, box, _) {
+              if (box.values.isEmpty) {
+                return const Center(
+                  child: Text("No contacts"),
+                );
+              }
+              return ListView.builder(
+                  itemCount: box.values.length,
+                  itemBuilder: (_, index) {
+                    Server curServer = box.getAt(index)!;
+                    return Card(
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.monitor_heart_rounded,
+                          size: 35.0,
+                          color: curServer.isConnected
+                              ? Colors.greenAccent
+                              : Colors.grey[700],
+                        ),
+                        title: Text(curServer.name),
+                        subtitle: Text(
+                            "IP : ${curServer.ipAddress}\nMac : ${curServer.macAddress}"),
+                        isThreeLine: true,
+                      ),
+                    );
+                  });
+            }),
       ),
       floatingActionButton: Builder(builder: (context) {
         return FloatingActionButton(
-          onPressed: () => _showBottomSheet(context),
+          onPressed: () => showServerBottomSheet(context),
           tooltip: "Add Server",
           child: const Icon(Icons.add),
         );
       }),
     );
-  }
-
-  _showBottomSheet(BuildContext context) {
-    final formKey = GlobalKey<FormState>();
-    final nameCon = TextEditingController();
-    final ipCon = TextEditingController();
-    void handleSubmit(BuildContext context) {
-      if (formKey.currentState!.validate()) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Adding data to database'),
-          ),
-        );
-        context.pop();
-        var serverBox = Hive.box<Server>("server");
-        Server newServer = Server(name: nameCon.text, ipAddress: ipCon.text);
-        serverBox.add(newServer);
-      }
-    }
-
-    return showBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            height: 400.00,
-            padding:
-                const EdgeInsets.symmetric(vertical: 60.0, horizontal: 15.0),
-            child: Form(
-              key: formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TextFormField(
-                    controller: nameCon,
-                    textCapitalization: TextCapitalization.sentences,
-                    keyboardType: TextInputType.name,
-                    autofocus: true,
-                    textInputAction: TextInputAction.next,
-                    decoration: const InputDecoration(labelText: "* Name"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter a name";
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    controller: ipCon,
-                    textCapitalization: TextCapitalization.none,
-                    keyboardType: TextInputType.text,
-                    textInputAction: TextInputAction.done,
-                    decoration:
-                        const InputDecoration(labelText: "* Ip Address"),
-                    validator: (val) {
-                      RegExp valRe =
-                          RegExp(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}:\d{4}$');
-                      if (val != null && valRe.hasMatch(val)) {
-                        return null;
-                      }
-                      return "Please enter valid Ip";
-                    },
-                    onEditingComplete: () => handleSubmit(context),
-                  ),
-                  const SizedBox(height: 20.0),
-                  OutlinedButton(
-                    onPressed: () {
-                      handleSubmit(context);
-                    },
-                    child: const Text("submit"),
-                  )
-                ],
-              ),
-            ),
-          );
-        });
   }
 }
