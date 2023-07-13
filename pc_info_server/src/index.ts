adec72e00e2b7d5ba9b0c73f6ae522ec52b8170f
Syed Devop <syeds.devops007@gmail.com>
2023-07-13 22:18:31 +0530

    feat(server): add client call getMac function
     - add getMacAddress function

--- pc_info_server/src/index.ts
+++ pc_info_server/src/index.ts
@@ -1,12 +1,14 @@
 import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import { NodeAudioVolumeMixer as volume } from "node-audio-volume-mixer";
+import os from "os";
 const PORT = 3000;
 
 interface ServerToClientEvents {}
 
 interface ClientToServerEvents {
   isMute: (_: null, cal: (mute: boolean) => void) => void;
+  getMac: (_: null, cal: (mac: string) => void) => void;
 }
 
 interface InterServerEvents {
@@ -37,12 +39,24 @@ instrument(io, {
 
 io.on("connection", (socket) => {
   console.log(`socket ${socket.id} connected`);
-  const vol = volume.isMasterMuted();
+
   socket.on("isMute", (_, cal) => {
+    const vol = volume.isMasterMuted();
     cal(vol);
   });
-
+  socket.on("getMac", (_, cal) => cal(getMacAddress()));
   socket.on("disconnect", (reason) => {
     console.log(`socket ${socket.id} disconnected due to ${reason}`);
   });
 });
+function getMacAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]!) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.mac;
+      }
+    }
+  }
+  return "";
+}
