7188a072b07156305903895030cf9af3c5f2a49d
syed Devop <syeds.devops007@gmail.com>
2023-07-26 20:35:57 +0530

    faet(app): add onpress function on utils card

--- pu_info_app/lib/widget/utils/utils_cards.dart
+++ pu_info_app/lib/widget/utils/utils_cards.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
 
 class UtilCard extends StatelessWidget {
-  const UtilCard({
-    super.key,
-    required this.icon,
-    required this.footer,
-  });
+  const UtilCard(
+      {super.key, required this.icon, required this.footer, this.onPress});
 
   final IconData icon;
   final String footer;
-
+  final Function()? onPress;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Expanded(
-            child: Icon(
-              icon,
-              size: 40,
+    return InkWell(
+      onTap: onPress,
+      child: Card(
+        child: Column(
+          children: [
+            Expanded(
+              child: Icon(
+                icon,
+                size: 40,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              footer,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                footer,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
