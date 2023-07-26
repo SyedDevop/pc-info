7e0536d8b8ba748291bda704fc3d84dad41c9665
syed Devop <syeds.devops007@gmail.com>
2023-07-26 20:40:25 +0530

    WIP(app): custom audio nob.

---
+++ pu_info_app/lib/widget/ui/audio_nob.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AudioNob extends StatelessWidget {
+  const AudioNob({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class AudioNobPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
+}
